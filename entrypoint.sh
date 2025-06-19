7b370647d0ab27b63e943f68bb5e271cdb1ab320
DevKevscoding <166475973+DevKevscoding@users.noreply.github.com>
2025-06-19 11:41:39 +0300

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "🚀 Démarrage de Odoo avec nettoyage cache + dev mode"
+
+# Nettoyage des fichiers cache/filestore cassés
+rm -rf /var/lib/odoo/filestore/* /var/lib/odoo/.local/share/Odoo/filestore/* || true
+rm -rf /var/lib/odoo/.local/share/Odoo/web/assets/* || true
+
+exec odoo \
+  -d railway \
+  -i base \
+  --dev=all \
+  --db_host=dpg-postgres.railway.internal \
+  --db_port=5432 \
+  --db_user=postgres \
+  --db_password=NSmKccDkFElZmmiKnstQAIYTQeFYOotk \
+  --http-port=8069 \
+  --without-demo=all \
+  --log-level=info \
